537a30d3ce4faefc214a007cf5b803a8a75c1677
vladislav <vladnovoren@gmail.com>
2022-04-17 17:53:21 +0300

    added UnsafeSharedObject

---
+++ include/unsafe_shared_object.hpp
@@ -0,0 +1,27 @@
+#ifndef UNSAFE_SHARED_OBJECT_HPP
+#define UNSAFE_SHARED_OBJECT_HPP
+
+#include "shared_memory_object.hpp"
+
+template <typename ObjT>
+class UnsafeSharedObject {
+public:
+  template <typename... ArgsT>
+  UnsafeSharedObject(const std::string &name, ArgsT &&... args)
+      : shared_memory_(name, sizeof(ObjT)) {
+    ObjT* obj_ptr = static_cast<ObjT*>(shared_memory_.Data());
+    new (obj_ptr) ObjT(std::forward<ArgsT>(args)..);
+  }
+
+  ~UnsafeSharedObject() {}
+
+  ObjT* GetObjPtr() {
+    return static_cast<ObjT*>(shared_memory_.Data());
+  }
+
+private:
+  SharedMemoryObject shared_memory_;
+
+};
+
+#endif /* unsafe_shared_object.hpp */
